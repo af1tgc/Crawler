a3f0e974aba7730219c809cf4bf03005d07646ab
af1tgc <af1tgc@gmail.com>
2019-08-12 07:30:06 +0000

    190812_add_github.com/PuerkitoBio/goquery

--- crawler.go
+++ crawler.go
@@ -3,9 +3,9 @@ package main
 import (
 //    "fmt"
     "log"
-//    "io/ioutil"
     "net/http"
     "html/template"
+    "github.com/PuerkitoBio/goquery"
 )
 
 type Page struct {
@@ -34,6 +34,7 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
     renderTemplate(w, "root", p)
 }
 
+
 //TODO: Get Url to Crawling
 func getCrawlUrlHandler(w http.ResponseWriter, r *http.Request){
     title := r.URL.Path[len("/getUrl"):]
@@ -45,11 +46,45 @@ func getCrawlUrlHandler(w http.ResponseWriter, r *http.Request){
 func backCrawlHandler(w http.ResponseWriter, r *http.Request){
     title := "Go Ahead"
     body := r.FormValue("body")
-    p := &Page{Title: title, Body: []byte(body)}
+    test := Crawl(body)
+    p := &Page{Title: title, Body: []byte(test)}
+    
     renderTemplate(w, "page/back_run", p)
 }
 
 
+//TODO: web-crawl
+func Crawl(url string) string {
+    test := ""
+//    res, err := http.Get(url)
+    res, err := http.Get("http://metalsucks.net")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer res.Body.Close()
+    if res.StatusCode != 200 {
+        log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+    }
+
+    doc, err := goquery.NewDocumentFromReader(res.Body)
+    if err != nil {
+        log.Fatal(err)
+    }
+/*
+    doc.Find(".col-md-9 content").Each(func(i int, s *goquery.Selection){
+        test = s.Find("h3").Text()
+    })
+
+    return test
+*/
+
+  doc.Find(".sidebar-reviews article .content-block").Each(func(i int, s *goquery.Selection) {
+    band := s.Find("a").Text()
+    title := s.Find("i").Text()
+    test = "Review " + band + title
+  })
+    return test
+}
 
 //TODO: Render the Tag lists for selection
 //TODO: Save to Files (csv) || Insert to Database
---
+++ vim-crawler.sh
@@ -0,0 +1,1 @@
+vim crawler.go
